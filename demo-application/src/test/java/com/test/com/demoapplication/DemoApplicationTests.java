3de8897398de7a3815f484e1323d53c2b4990756
Vaibhav Rodage <vrodage001@gmail.com>
2021-05-31 18:08:10 +0530

    updated test case

--- demo-application/src/test/java/com/test/com/demoapplication/DemoApplicationTests.java
+++ demo-application/src/test/java/com/test/com/demoapplication/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.test.com.demoapplication;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class DemoApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.assertTrue(true);
     }
 
 }
